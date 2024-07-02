247c2c86a7896cb6177ba9a958b8303742c3e9e2
idkac <andrewsun71@gmail.com>
2024-07-01 18:23:53 -0700

    added name

--- proc_count.c
+++ proc_count.c
@@ -27,6 +27,6 @@ static void __exit proc_count_exit(void)
 module_init(proc_count_init);
 module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Your Name");
+MODULE_AUTHOR("Andrew Sun 006159649");
 MODULE_DESCRIPTION("CS111 lab0 count proc number");
 MODULE_LICENSE("GPL");
